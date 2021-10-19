df2701e35fed42fa4b580f18d41a1b33d1144ac1
izuru775 <izuru775@gmail.com>
2021-10-19 22:18:33 +1100

    Some packages added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
 var express = require("express");
+const { dirname } = require('path');
 var path = require("path");
 app =express();
+let http = require('http').createServer(app);
 
-app.use(express.static(__dirname +"/public"));
 app.get('/',(req,res)=>{
-    res.status(200).sendFile(__dirname,'public','Ch1','01_02','example.html');
-})
-app.listen(3000);
+    res.sendFile(path.join(__dirname,'public','index.html'))
+});
+app.use(express.static(__dirname +"/public"));
+var port = process.env.PORT|| 3000;
+
+http.listen(port,()=>{
+    console.log("listneing on port...",port);
+});
